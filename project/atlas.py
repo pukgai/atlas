a025ba39351a1fa17444c5530e513ee9b6b4dca8
pukgai <eric.ng1984@gmail.com>
2016-05-19 16:42:14 -0700

    Initialized project and manual/datasheet directories
    Initialized project and manual/datasheet directories.  Populated
    directories with manuals, datasheets, and initial atlas.py, as well as
    lib/keithley2400.py

---
+++ project/atlas.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Atlas 
+Writen By Eric NG
+Updated : 5/19/2016
+"""
+import os
+from lib import keithley2400
+
+#if os.path.isfile(keithley):
+#   print "true"
+#else:
+#    print "false"
+
